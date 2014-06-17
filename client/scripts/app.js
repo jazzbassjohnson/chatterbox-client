b38357fab449b3579cc72aae5304ada731f4df80
jazzbassjohnson <jazzbassjohnson@gmail.com>
2014-06-16 17:06:11 -0700

    Added input box and allow user to send messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,72 +1,95 @@
 // YOUR CODE HERE:
+$(document).ready(function(){
 
-var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox',
-  init: function(){
-    this.username = window.location.search.slice(10);
-    this.fetch();
-    var self = this;
-    setInterval(function() {
-      self.clearMessages.call(self);
-      self.fetch.call(self);
-    }, 5000);
-  },
-  send: function(message){
-    $.ajax({
-      type: 'POST',
-      url: this.server,
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function(){
-        console.log('message sent');
-      },
-      error: function(){
-        console.error('Error sending message');
+  var app = {
+    server: 'https://api.parse.com/1/classes/chatterbox',
+    init: function(){
+      this.username = window.location.search.slice(10);
+      this.fetch();
+      var self = this;
+      setInterval(function() {
+        self.clearMessages.call(self);
+        self.fetch.call(self);
+      }, 5000);
+    },
+    send: function(message){
+      $.ajax({
+        type: 'POST',
+        url: this.server,
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function(){
+          console.log('message sent');
+        },
+        error: function(){
+          console.error('Error sending message');
+        }
+      });
+    },
+    fetch: function(){
+      var self = this;
+      $.ajax({
+        type: 'GET',
+        url: this.server,
+        contentType: 'application/json',
+        data: {order: '-createdAt'},
+        success: function(data){
+
+          self.renderMessages(data.results);
+        },
+        error: function(){
+          console.error('Error: fetching messages');
+        }
+
+      });
+    },
+    addMessage: function(messageObject){
+      var $messages =  $('#chats');
+      var $messageContainer = $('<li class="chat">');
+      var $currentMessage = $('<span>');
+      var $userName = $('<span class= "username">');
+      $userName.text(messageObject.username);
+      $currentMessage.text(': ' + messageObject.text);
+      $messageContainer.append($userName);
+      $messageContainer.append($currentMessage);
+      $messages.append($messageContainer);
+
+    },
+    renderMessages: function(messageArray){
+      for(var i = 0; i<messageArray.length; i++){
+        this.addMessage(messageArray[i]);
       }
-    });
-  },
-  fetch: function(){
-    var self = this;
-    $.ajax({
-      type: 'GET',
-      url: this.server,
-      contentType: 'application/json',
-      data: {order: '-createdAt'},
-      success: function(data){
-
-        self.renderMessages(data.results);
-      },
-      error: function(){
-        console.error('Error: fetching messages');
-      }
-
-    });
-  },
-  addMessage: function(messageObject){
-    var $messages =  $('#chats');
-    var $messageContainer = $('<li class="chat">');
-    var $currentMessage = $('<span>');
-    var $userName = $('<span class= "username">');
-    $userName.text(messageObject.username);
-    $currentMessage.text(': ' + messageObject.text);
-    $messageContainer.append($userName);
-    $messageContainer.append($currentMessage);
-    $messages.append($messageContainer);
-
-  },
-  renderMessages: function(messageArray){
-    for(var i = 0; i<messageArray.length; i++){
-      this.addMessage(messageArray[i]);
+    },
+    clearMessages: function(){
+      $('#chats').empty();
+    },
+    addRoom: function(roomName){
+      var $room = $('<li class="room">');
+      $room.text(roomName);
+      $('#roomSelect').append($room);
     }
-  },
-  clearMessages: function(){
-    $('#chats').empty();
-  },
-  addRoom: function(roomName){
-    var $room = $('<li class="room">');
-    $room.text(roomName);
-    $('#roomSelect').append($room);
-  }
-};
+  };
 
-app.init();
+  app.init();
+
+  $('button').on('click', function() {
+    var message = {};
+    message.username = app.username;
+    message.text = $('#userMessage').val();
+    $('#userMessage').val('');
+    message.roomname = '';
+    app.send(message);
+  });
+
+  $('#userMessage').keypress(function(event){
+    if (event.which === 13) {
+      var message = {};
+      message.username = app.username;
+      message.text = $(this).val();
+      $(this).val('');
+      message.roomname = '';
+      app.send(message);
+    }
+  });
+
+});
